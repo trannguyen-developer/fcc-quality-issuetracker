a6ef47122d6f40b1529c328f26783bfa57a56a81
Nguyen Tran <trannguyenhn2000@gmail.com>
2025-09-23 23:50:01 +0700

    feat: design schema issue

--- connect.js
+++ connect.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // Thay link dưới bằng connection string của bạn
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- issueSchema.js
+++ issueSchema.js
@@ -1,24 +1,22 @@
 const mongoose = require("mongoose");
 
-const issueSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    age: {
-      type: Number,
-      default: 18,
-    },
+const issueSchema = new mongoose.Schema({
+  issue_title: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  issue_text: {
+    type: String,
+    required: true,
+  },
+  created_by: String,
+  assigned_to: String,
+  open: { type: Boolean, default: true },
+  status_text: String,
+  created_on: { type: Date, default: Date.now() },
+  timestamps: { createdAt: "created_on", updatedAt: "updated_on" },
+});
+
+// const User = mongoose.model('User', userSchema);
 
 module.exports = issueSchema;
