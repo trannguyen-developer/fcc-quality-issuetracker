179861fdb046ce781b2272a7ca9fff1dad680e9f
Nguyen Tran <trannguyenhn2000@gmail.com>
2025-09-24 00:12:08 +0700

    feat: create issues

--- issueSchema.js
+++ issueSchema.js
@@ -1,21 +1,23 @@
 const mongoose = require("mongoose");
 
-const issueSchema = new mongoose.Schema({
-  issue_title: {
-    type: String,
-    required: true,
+const issueSchema = new mongoose.Schema(
+  {
+    issue_title: {
+      type: String,
+      required: true,
+    },
+    issue_text: {
+      type: String,
+      required: true,
+    },
+    created_by: String,
+    assigned_to: String,
+    open: { type: Boolean, default: true },
+    status_text: String,
+    created_on: { type: Date, default: Date.now() },
   },
-  issue_text: {
-    type: String,
-    required: true,
-  },
-  created_by: String,
-  assigned_to: String,
-  open: { type: Boolean, default: true },
-  status_text: String,
-  created_on: { type: Date, default: Date.now() },
-  timestamps: { createdAt: "created_on", updatedAt: "updated_on" },
-});
+  { timestamps: { createdAt: "created_on", updatedAt: "updated_on" } }
+);
 
 // const User = mongoose.model('User', userSchema);
 
--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,30 @@
 "use strict";
+const mongoose = require("mongoose");
+const userSchema = require("../issueSchema");
 
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
-    .get(function (req, res) {
+
+    .get(async function (req, res) {
       let project = req.params.project;
     })
 
-    .post(function (req, res) {
-      let project = req.params.project;
+    .post(async function (req, res) {
+      try {
+        let project = req.params.project;
+        if (!project) {
+          return res.json({ error: "wrong project" });
+        }
+        const Issue = mongoose.model("project", userSchema);
+
+        const issue = new Issue(req.body);
+        await issue.save();
+        res.json({ data: issue });
+      } catch (error) {
+        console.log("error", error);
+        res.json({ error });
+      }
     })
 
     .put(function (req, res) {
